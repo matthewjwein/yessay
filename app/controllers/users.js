7ffed20704e2e965bc932f7b6c5fa0353df0f26b
Matthew Wein <mwein2009@gmail.com>
2013-08-02 17:54:34 -0700

    updated email

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -84,10 +84,8 @@ exports.create = function (req, res) {
       sendgrid.send({
         to: user.email,
         from: 'jacqueline@yessay.com',
-        fromname: 'Jacqueline Abrams',
         subject: 'Thanks for signing up with Yessay!',
-        text: 'Thanks for signing up with Yessay!' +
-          'You’ve got a big year ahead of you with college applications, ' +
+        text:'Thanks for signing up with Yessay!You’ve got a big year ahead of you with college applications, ' +
           'and we want to make the journey a little smoother. So, we’re sharing ' +
           'our best-kept secrets on writing a truly kickass admissions essay. ' +
           'These insights come from years of teaching writing at NYU and Emory University, ' +
@@ -98,8 +96,11 @@ exports.create = function (req, res) {
           '\n\nJacqueline Abrams' +
           '\n\nSign in to your account: www.yessay.com/login' +
           'Username: ' + user.email
-      }, function(err) {
-        return err
+      }, function(success, message) {
+        if (!success) {
+          console.log(message)
+          return res.redirect('/users/'+user.id)
+        }
       });
 
       return res.redirect('/users/'+user.id)
